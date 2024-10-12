785e1a0b9435fc90003001d5780f44f47a28efb7
jaehee <jaehee.shin@duke.edu>
2024-10-12 16:23:04 -0400

    button

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -6,22 +6,41 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand></Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/step1">
-              Step1
-            </Nav.Link>
-            <Nav.Link as={Link} to="/step2">
-              Step2
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    // <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    //   <Container>
+    //     <Navbar.Brand></Navbar.Brand>
+    //     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    //     <Navbar.Collapse id="responsive-navbar-nav">
+    //       {/* Center the Nav Links using justify-content-center */}
+    //       <Nav className="justify-content-center w-100">
+    //         <Nav.Link as={Link} to="/Step1">
+    //           Step1
+    //         </Nav.Link>
+    //         <Nav.Link as={Link} to="/Step2">
+    //           Step2
+    //         </Nav.Link>
+    //       </Nav>
+    //     </Navbar.Collapse>
+    //   </Container>
+    // </Navbar>
+
+    <nav className="fixed bottom-0 left-0 right-0  p-4">
+      <div className="container mx-auto flex justify-center items-center">
+        <div className= "flex space-x-8">
+          <Nav.Link as={Link} to="/Step1"
+          className = "bg-gray-500 hover:bg-purple-900 text-white font-bold w-20 h-20 rounded-full flex items-center justify-center transition duration-300">
+          
+               Search
+             </Nav.Link>
+          <Nav.Link as={Link} to="/Step2"
+          className="bg-gray-500 hover:bg-purple-900 text-white font-bold w-20 h-20 rounded-full flex items-center justify-center transition duration-300"
+          >
+               Analyze
+             </Nav.Link>
+        </div>
+      </div>
+    </nav>
+
   );
 };
 
